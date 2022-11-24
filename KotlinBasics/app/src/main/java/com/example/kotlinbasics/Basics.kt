c6679fc1cf1628d362ce6a324cf90f29f75591ff
Taylor Jensen <Taylorlcj@gmail.com>
2022-11-24 11:38:11 -0500

    when statements

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -83,6 +83,61 @@ fun main(){
         println("Wait, you're not Bob!!")
     }
 
+    //when statements
+    var season = 3
+    when(season){
+        1 -> println("Spring")
+        2 -> println("Summer")
+        3 -> {
+            println("fall")
+            println("Autumn")
+        }
+        4 -> println("winter")
+        else -> println("Invalid season")
+    }
+
+    var month = 3
+    when(month){
+        in 3..5 -> println("Spring")
+        in 6..8 -> println("Summer")
+        in 9..11 -> println("Fall")
+        12, 1, 2 -> println("Winter")
+        else -> println("Invalid season")
+    }
+
+    var x : Any = 13.37
+    when(x){
+        is Int -> println("$x is an Int")
+        !is Double -> println("$x is not a Double")
+        is String -> println("$x is a String")
+        else -> println("$x is none of the above")
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
