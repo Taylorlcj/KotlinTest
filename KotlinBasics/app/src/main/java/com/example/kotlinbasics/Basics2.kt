a7eb721a5ad296b495fff6d5b179cb72302a1a99
Taylor Jensen <Taylorlcj@gmail.com>
2022-12-11 23:58:28 -0500

    functions

---
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics2.kt
@@ -0,0 +1,20 @@
+package com.example.kotlinbasics
+
+fun main() {
+    val result = addUp(5,3)
+    println("result is $result")
+    val avg = average(5.3,13.37)
+    println("result is $avg")
+}
+//Method - a function within a class
+// Parameters (input)
+fun addUp(a: Int, b: Int,) : Int{
+    return a + b
+}
+
+fun average(a: Double, b: Double,) : Double{
+    return (a + b) / 2
+}
+fun myFunction(){
+    println("Called from myFunction")
+}
