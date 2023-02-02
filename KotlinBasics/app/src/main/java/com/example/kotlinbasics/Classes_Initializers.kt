d9d20ab64c616a1ac5bdea3d155ebe1e5365de20
Taylor Jensen <Taylorlcj@gmail.com>
2023-02-01 20:54:53 -0500

    fixed them

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/Classes_Initializers.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/Classes_Initializers.kt
@@ -1,9 +1,12 @@
 package com.example.kotlinbasics
 
 fun main(){
-
+    var denis: Person = Person("Tayor", "Jensen")
 }
 
 class Person (firstName: String, lastName: String){
 
+    init {
+        println("Person Created") //initialized a new person object
+    }
 }
